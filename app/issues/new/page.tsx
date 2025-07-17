526ee809dedb48c8cbcc2c93697fb80869cbaf2c
Evgin Aydın <evgin2004@gmail.com>
2025-07-17 11:35:22 +0300

    Implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,22 +2,29 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import SimpleMDE from "react-simplemde-editor";
+import { Textarea } from "@/components/ui/textarea";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -39,12 +46,25 @@ const NewIssuePage = () => {
           }
         })}
       >
+        {errors.title && (
+          <Alert variant="destructive" className="mb-5">
+            <AlertTitle>Error!</AlertTitle>
+            <AlertDescription>{errors.title.message}</AlertDescription>
+          </Alert>
+        )}
         <Input placeholder="Title" {...register("title")} />
+
+        {errors.description && (
+          <Alert variant="destructive" className="mb-5">
+            <AlertTitle>Error!</AlertTitle>
+            <AlertDescription>{errors.description.message}</AlertDescription>
+          </Alert>
+        )}
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
+            <Textarea placeholder="Description" {...field} />
           )}
         />
         <Button>Submit New Issue</Button>

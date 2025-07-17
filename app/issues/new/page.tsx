40ceb1b1c3bddeb5ac46ded27660b21e8af1b655
Evgin Aydın <evgin2004@gmail.com>
2025-07-17 13:26:34 +0300

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -29,6 +29,16 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred");
+    }
+  });
   return (
     <div className="max-w-xl">
       {error && (
@@ -37,19 +47,7 @@ const NewIssuePage = () => {
           <AlertDescription>{error}</AlertDescription>
         </Alert>
       )}
-      <form
-        className=" space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpected error occurred");
-          }
-        })}
-      >
+      <form className=" space-y-3" onSubmit={onSubmit}>
         {errors.title && (
           <Alert variant="destructive" className="mb-5">
             <AlertTitle>Error!</AlertTitle>
